611a6ad9dbc96ff47c37256d383b083f2ec1a31d
P-Silva1974746 <pedrofsilva5164@gmail.com>
2024-03-04 12:49:07 +0000

    create file for the a* implemention

---
+++ connectfour/a_start.py
@@ -0,0 +1,13 @@
+def heuristic(board, player):
+    for row in range (len(board)-1,0,-1): #search 
+        for col in (0,len(board),1):
+            if(board[row][col]==' '):
+
+
+
+
+def a_star (board,player):
+    row=-1
+
+
+    return row
